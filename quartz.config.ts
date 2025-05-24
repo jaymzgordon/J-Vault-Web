da113c1e0110ed454478005496322db9510360e5
jaymzgordon <120748200+jaymzgordon@users.noreply.github.com>
2025-05-24 15:44:43 +0100

    Quartz sync: May 24, 2025, 3:44 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🎵 JG's Vault",
+    pageTitle: "JG's Vault",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
