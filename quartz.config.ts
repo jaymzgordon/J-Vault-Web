14e3ee6f6cd494f0cf52ce89e0c7587cc2aed595
jaymzgordon <120748200+jaymzgordon@users.noreply.github.com>
2025-05-24 16:35:35 +0100

    Quartz sync: May 24, 2025, 4:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,6 +1,5 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
-import { CustomOgImages } from "./quartz/plugins/emitters/ogImage"
 
 /**
  * Quartz 4.0 Configuration
