ff685a46bb0b512ab0f3fdb4b23e51f693dc2766
jaymzgordon <120748200+jaymzgordon@users.noreply.github.com>
2025-05-24 16:58:00 +0100

    Quartz sync: May 24, 2025, 4:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,7 +19,7 @@ const config: QuartzConfig = {
     baseUrl: "jaymzgordon.github.io/J-Vault-Web",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
-    generateSocialImages: false,
+    generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
