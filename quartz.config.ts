a6fc18ba323c0ea28d4625444f25637192b09845
jaymzgordon <120748200+jaymzgordon@users.noreply.github.com>
2025-05-25 13:12:30 +0100

    Quartz sync: May 25, 2025, 1:12 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "JG's Vault",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
@@ -19,12 +19,6 @@ const config: QuartzConfig = {
     baseUrl: "jaymzgordon.github.io/J-Vault-Web",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
-    generateSocialImages: {
-      colorScheme: "darkMode", // what colors to use for generating image, same as theme colors from config, valid values are "darkMode" and "lightMode"
-      width: 1200, // width to generate with (in pixels)
-      height: 630, // height to generate with (in pixels)
-      excludeRoot: false, // wether to exclude "/" index path to be excluded from auto generated images (false = use auto, true = use default og image)
-    },
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
