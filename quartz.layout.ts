eeda5f27dc766e71b6423119abc79ae47a2f229c
jaymzgordon <120748200+jaymzgordon@users.noreply.github.com>
2025-05-26 16:37:59 +0100

    Quartz sync: May 26, 2025, 4:37 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,7 +31,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.RecentNotes({ limit: 5 })),
-    Component.DesktopOnly(Component.Explorer()),
+    // Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     Component.Graph(),
